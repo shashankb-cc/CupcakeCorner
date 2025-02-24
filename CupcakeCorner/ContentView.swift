53429ffa583c646a8b00b1a80a67164675906dd3
shashankb-cc <shashank.b@codecraft.co.in>
2025-02-24 11:56:48 +0530

    handling the diable form

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,57 +7,27 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
-    @State private var results = [Result]()
-    var body: some View {
-        List(results,id: \.trackId) { item in
-            VStack(alignment:.leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
-            }
-        }
-        .task {
-            await loadData()
-             
-        }
-        AsyncImage(url: URL(string: "https://d1wyjpgk9j1ia3.cloudfront.net/default-profile/default-avatar.png")) { phase in
-            if let image = phase.image {
-                image
-                    .resizable()
-                    .scaledToFit()
-            } else if phase.error != nil {
-                Text("There was an error loading the image.")
-            } else {
-                ProgressView()
-            }
-        }
-
+    @State private var username = ""
+    @State private var password = ""
+    
+    var disableForm: Bool {
+        username.count < 5 || password.count < 5
     }
-    func loadData() async {
-        
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid result")
-            return
-        }
-        do {
-            let (data,_) = try await URLSession.shared.data(from: url)
-            
-            if let decodedResponse = try? JSONDecoder().decode(Response.self,from: data) {
-                results = decodedResponse.results
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Enter you username", text: $username)
+                TextField("Enter your password", text: $password)
             }
-        } catch {
-            print("Some error occured",error)
+            Section {
+                Button("Create Account") {
+                    print("Creating account")
+                }
+            }
+//            .disabled(username.isEmpty || password.isEmpty)
+            .disabled(disableForm)
         }
     }
 }
